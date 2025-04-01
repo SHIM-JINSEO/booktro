f2c2f6faacdf1452c7e0684afa791573fce4bce5
SHIM-JINSEO <sjs011828@gmail.com>
2025-04-02 03:17:38 +0900

    feat: adding return to mainpage

--- src/Components/nav.tsx
+++ src/Components/nav.tsx
@@ -1,9 +1,10 @@
 import { Search } from "lucide-react";
 import { Input } from "@/Components/ui/input";
+import { Link } from "@tanstack/react-router";
 export default function Nav() {
   return (
     <div className="flex w-full items-center justify-between bg-white p-6 shadow-lg">
-      <h1 className="text-2xl font-bold">Booktro</h1>
+      <Link to="/"><h1 className="text-2xl font-bold">Booktro</h1></Link>
       <nav className="flex gap-x-4">
         <Input/>
         <Search/>
